c001bc0d72c1cf7129aa191ac94cd0e8013dec1a
TheWitheredStriker <withersoul235@gmail.com>
2022-12-27 19:23:54 +0100

    Rewrite date/time formats

--- script/date.js
+++ script/date.js
@@ -1,7 +1,7 @@
 const now = new Date();
 
 document.querySelector(".date").innerHTML = 
-  now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate();
+  now.getDate() + '-' + (now.getMonth() + 1) + '-' + now.getFullYear();
 
 // Admittedly a bit of a hack because the browser simply refuses not to display the seconds, no matter what.
 // Trust me, I tried. Not defining seconds at all doesn't help.
