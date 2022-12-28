6e85941e13251ff73cb7d40aebccd412c421ba27
TheWitheredStriker <withersoul235@gmail.com>
2022-12-28 10:28:43 +0100

    Add more crap I forgot to commit earlier

--- script/date.js
+++ script/date.js
@@ -1,14 +1,18 @@
-const now = new Date();
+setInterval((function () {
 
-document.querySelector(".date").innerHTML = 
-  now.getDate() + '-' + (now.getMonth() + 1) + '-' + now.getFullYear();
+  const now = new Date();
 
-// Admittedly a bit of a hack because the browser simply refuses not to display the seconds, no matter what.
-// Trust me, I tried. Not defining seconds at all doesn't help.
+  document.querySelector(".date").innerHTML = 
+    now.getDate() + '-' + (now.getMonth() + 1) + '-' + now.getFullYear();
 
-document.querySelector(".time").innerHTML = now.toLocaleTimeString(
-  "nl-BE", {
-      hours: "2-digit",
-      minutes: "2-digit"
-  }
-).slice(0, -3);
+  // Admittedly a bit of a hack because the browser simply refuses not to display the seconds, no matter what.
+  // Trust me, I tried. Not defining seconds at all doesn't help.
+
+  document.querySelector(".time").innerHTML = now.toLocaleTimeString(
+      "nl-BE", {
+        hours: "2-digit",
+        minutes: "2-digit"
+    }
+  ).slice(0, -3)
+
+}), 1000);
